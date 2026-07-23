f9f3cc369955f7e84449d5c140742e3474adc736
Aryan Ojha <2k23.csds2311504@gmail.com>
2026-07-23 20:57:22 +0530

    Time: 11 ms (17.13%) | Memory: 20.5 MB (23.67%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,7 +1,7 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         if len(nums)==1:
-            return
+            return 
         i=0
         while i<len(nums):
             if nums[i]==0:
@@ -15,4 +15,4 @@ class Solution:
                 nums[i],nums[j]=nums[j],nums[i]
                 i+=1
             j+=1
-            
+        
